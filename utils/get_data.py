d0501d37028a9586fdd209e6cbab6cd7df641d08
Zack's PC <zapulliam22@gmail.com>
2023-06-06 21:47:31 -0500

    update utils

--- utils/get_data.py
+++ utils/get_data.py
@@ -6,17 +6,9 @@ import pandas as pd
 
 
 def get_data(args):
-    frequency, info, folder = args.frequency, args.info, ''
+    info, folder = args.info, 'daily_prices'
 
-    if frequency == 'daily':
-        url = 'https://query1.finance.yahoo.com/v7/finance/download/{x}?period1=1513036800&period2=1670803200&interval=1d&events=history&includeAdjustedClose=true'
-        folder = 'daily_prices'
-    elif frequency == 'weekly':
-        url = 'https://query1.finance.yahoo.com/v7/finance/download/{x}?period1=1513036800&period2=1670803200&interval=1wk&events=history&includeAdjustedClose=true'
-        folder = 'weekly_prices'
-    elif frequency == 'monthly':
-        url = 'https://query1.finance.yahoo.com/v7/finance/download/{x}?period1=1513036800&period2=1670803200&interval=1mo&events=history&includeAdjustedClose=true'
-        folder = 'monthly_prices'
+    url = 'https://query1.finance.yahoo.com/v7/finance/download/{x}?period1=1513036800&period2=1670803200&interval=1d&events=history&includeAdjustedClose=true'
 
     dir = os.getcwd()
     dir = dir.split(os.sep)
@@ -42,7 +34,6 @@ def get_data(args):
     
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--frequency', type=str, default='weekly', choices=['daily', 'weekly', 'monthly'], help='frequency of price reporting')
     parser.add_argument('--info', type=str, default='S&P500-Info.csv', help='location of S&P500-Info.csv')
     args = parser.parse_args()
     return args
--- utils/get_info.py
+++ utils/get_info.py
@@ -1,3 +1,5 @@
+""" Purpose: creates csv file with info on all S&P 500 stocks, including ticker symbols"""
+
 import ssl
 import argparse
 import pandas as pd
@@ -6,7 +8,7 @@ from urllib import request
 
 
 def get_info(args):
-    url, info, tickers, = args.url, args.info, args.tickers
+    url, info, = args.url, args.info
 
     context = ssl._create_unverified_context()
     response = request.urlopen(url, context=context)
@@ -21,7 +23,6 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--url', type=str, default='https://en.wikipedia.org/wiki/List_of_S%26P_500_companies', help='wiki url of Fortune 500 companies')
     parser.add_argument('--info', type=str, default='S&P500-Info.csv', help='wiki url of Fortune 500 companies')
-    parser.add_argument('--tickers', type=str, default='S&P500-Symbols.csv', help='wiki url of Fortune 500 companies')
     args = parser.parse_args()
     return args
 
