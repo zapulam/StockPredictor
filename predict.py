9b9640ca165e3a5856dacfbd880f06d8fbb5e219
Zack's PC <zapulliam22@gmail.com>
2023-06-11 14:05:40 -0500

    minor changes to args

--- predict.py
+++ predict.py
@@ -9,24 +9,27 @@ from rnn import LSTM
 
 
 def predict(args):
-    weights, steps, device, savepath = \
-        args.weights, args.steps, args.device, args.savepath
-    
-    os.mkdir(os.path.join('predictions', savepath))
-
-    url = 'https://query1.finance.yahoo.com/v7/finance/download/{x}?period1=1513036800&period2=1670803200&interval=1d&events=history&includeAdjustedClose=true'
+    weights, skip, steps, device, savepath = \
+        args.weights, args.skip, args.steps, args.device, args.savepath
 
     df = pd.read_csv('utils/S&P500-Info.csv')
     symbols = df['Symbol'].tolist()
+    
+    if not skip:
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} # This is chrome, you can set whatever browser you like
 
-    # Update stock data to morst recent
-    for symbol in symbols:
-        if '.' in symbol:
-            symbol = symbol.replace('.', '-')
-        get = requests.get(url.format(x=symbol))
-        if get.status_code != 404:
-            data = pd.read_csv(url.format(x=symbol))
-            data.to_csv(os.path.join('daily_prices', symbol + '.csv'), index = False)
+        url = 'https://query1.finance.yahoo.com/v7/finance/download/{x}?period1=1528329600&period2=1686096000&interval=1d&events=history&includeAdjustedClose=true'
+
+        # Update stock data to morst recent
+        for symbol in symbols:
+            sys.stdout.write('\rGetting data for: %s' % symbol.ljust(4))
+            if '.' in symbol:
+                symbol = symbol.replace('.', '-')
+            get = requests.get(url.format(x=symbol), headers=headers)
+            if get.status_code != 404 & get.status_code != 400:
+                data = pd.read_csv(url.format(x=symbol))
+                data.to_csv(os.path.join('daily_prices', symbol + '.csv'), index = False)
+                sys.stdout.write('\rGetting data for: %s - DONE' % symbol.ljust(4))
 
     # Create unique save path for predictions
     k, newpath = 2, 'predictions/' + savepath
@@ -55,6 +58,8 @@ def predict(args):
         sys.stdout.write('\rPredicting prices for: %s' % stock.ljust(4))
 
         predictions = torch.rand(1,0,5)   # tensor to store future predictions
+        if 'cuda' in device:
+            predictions = predictions.cuda()
 
         data = pd.read_csv('daily_prices/' + stock + '.csv', index_col=0)
         x = data[['Open', 'High', 'Low', 'Volume', 'Close']]   # input data
@@ -67,6 +72,9 @@ def predict(args):
         
         mins = torch.tensor(mins.values)
         maxs = torch.tensor(maxs.values)
+
+        if 'cuda' in device:
+            x, mins, maxs = x.cuda(), mins.cuda(), maxs.cuda()
 
         for _ in range(steps):
             pred = model(x.float())   # model prediction for one time step
@@ -89,12 +97,14 @@ def predict(args):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='models/rnn/weights/best.pth', help='Path to model weights')
-    
+
+    parser.add_argument('--skip', action='store_true', help='Skip data download')
+
     parser.add_argument('--steps', type=int, default=25, help='Future time steps to predict')
 
     parser.add_argument('--device', type=str, default='cuda:0', help='device; cuda:n or cpu')
 
-    parser.add_argument('--savepath', type=str, help='Path to save the models (best and last)')
+    parser.add_argument('--savepath', type=str, help='Path to save predictions')
 
     args = parser.parse_args()
     return args
