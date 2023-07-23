52906b09c3d3917923a2b1d9c9564938ab565eb5
Zack's PC <zapulliam22@gmail.com>
2023-07-22 20:18:29 -0500

    fix cuda issue in train and add comments

--- predict.py
+++ predict.py
@@ -31,9 +31,11 @@ def predict(args):
     weights, skip, steps, device, savepath = \
         args.weights, args.skip, args.steps, args.device, args.savepath
 
+    # Get list of stock tickers
     df = pd.read_csv('utils/S&P500-Info.csv')
     symbols = df['Symbol'].tolist()
     
+    # Download most recent daily prices data before making predictions
     if not skip:
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} # this is chrome, you can set whatever browser you like
 
@@ -59,7 +61,7 @@ def predict(args):
                 data.to_csv(os.path.join('daily_prices', symbol + '.csv'), index = False)
                 sys.stdout.write('\rGetting data for: %s - DONE' % symbol.ljust(5))
 
-    # Create unique save path for predictions
+    # Create unique folder for predictions
     k, newpath = 2, 'predictions/' + savepath
     while True:
         if not os.path.isdir(newpath):
@@ -89,11 +91,13 @@ def predict(args):
         if 'cuda' in device:
             predictions = predictions.cuda()
 
+        # Load input data
         data = pd.read_csv('daily_prices/' + stock + '.csv', index_col=0)
-        x = data[['Open', 'High', 'Low', 'Volume', 'Close']]   # input data
+        x = data[['Open', 'High', 'Low', 'Volume', 'Close']] 
 
         mins, maxs = x.min(), x.max()   # values for normalization
 
+        # Normalize input data
         x = (x-mins)/(maxs-mins)
         x = torch.tensor(x.values)
         x = torch.unsqueeze(x, dim=0)
@@ -112,6 +116,7 @@ def predict(args):
 
             x = torch.cat((x, pred), dim=1)   # append predicition to input data for next time step
 
+        # Un-normalize input data and save
         predictions = predictions*(maxs-mins)+mins
         predictions = pd.DataFrame(predictions.cpu().squeeze().detach().numpy(), columns=['Open', 'High', 'Low', 'Volume', 'Close'])
         predictions.to_csv(os.path.join(newpath, stock + '.csv'), index = False)
--- train.py
+++ train.py
@@ -107,7 +107,9 @@ def train(args):
 
             # Train model for each sequence
             for _, seq in enumerate(seqs):
-                predictions = torch.rand(bs,0,5).cuda()   # tensor to store future predictions
+                predictions = torch.rand(bs,0,5)  # tensor to store future predictions
+                if 'cuda' in device:
+                    predictions = predictions.cuda()
 
                 x = seq[0].float()   # get inputs from seq
                 y = seq[1].float()   # get ground truth from seq
@@ -147,6 +149,8 @@ def train(args):
                 # Validate model for each sequence
                 for _, seq in enumerate(seqs):
                     predictions = torch.rand(bs,0,5)   # tensor to store future predictions
+                    if 'cuda' in device:
+                        predictions = predictions.cuda()
 
                     x = seq[0].float()   # get inputs from seq
                     y = seq[1].float()   # get ground truth from seq
